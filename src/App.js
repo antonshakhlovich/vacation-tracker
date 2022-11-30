5ac8653d16c0580b265503a1720bd740d9a594e9
Anton Shakhlovich <anton.shakhlovich@gmail.com>
2022-11-30 16:20:37 +0400

    feat: display balance for hardcoded user

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [vac, setVac] = useState({});
+
+
+  useEffect(() => {
+    const {
+      REACT_APP_GOOGLE_SHEET_ID, 
+      REACT_APP_GOOGLE_PRIVATE_KEY, 
+      REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL
+    } = process.env;
+    const doc = new GoogleSpreadsheet(REACT_APP_GOOGLE_SHEET_ID);
+
+    async function getData() {
+      await doc.useServiceAccountAuth({
+        client_email: REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: REACT_APP_GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
+      });
+      await doc.getInfo();
+      const sheet = doc.sheetsByIndex[0];
+      const rows = await sheet.getRows();
+      const personData = rows.find(x => x.email === "anton.shakhlovich@siliconmint.com");
+      setVac(personData)
+    }
+
+    getData();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <tbody>
+            <tr>
+              <td>{vac["name"]}</td>
+              <td>{vac.email}</td>
+              <td>{vac.vacBalance}</td>
+            </tr>
+          </tbody>
+        </table>
       </header>
     </div>
   );
