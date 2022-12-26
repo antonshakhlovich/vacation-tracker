879869517fb35b0799778a20b8f709d370a726d1
Anton Shakhlovich <anton.shakhlovich@gmail.com>
2022-12-26 18:43:50 +0400

    feat: add simple logging to userData

--- src/functions/index.js
+++ src/functions/index.js
@@ -4,115 +4,108 @@ const functions = require('@google-cloud/functions-framework');
 
 // env variables
 const environment = process.env.ENV || 'development';
-if(environment === 'development') {
-    require('dotenv').config();
+if (environment === 'development') {
+  require('dotenv').config();
 }
 
 // Main Function
 functions.http('userData', (req, res) => {
-    // CORS enable
-    res.set('Access-Control-Allow-Origin', '*');
+  // CORS enable
+  res.set('Access-Control-Allow-Origin', '*');
 
-    if (req.method === 'OPTIONS') {
-        // Send response to OPTIONS requests
-        res.set('Access-Control-Allow-Methods', 'GET');
-        res.set('Access-Control-Allow-Headers', 'Content-Type');
-        res.set('Access-Control-Max-Age', '3600');
-        res.status(204).send('');
-        return;
-    }
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+    return;
+  }
 
-    getClient()
-    .then(client => getUserEmailbyToken(client, req.body.id_token || req.query.id_token))
-    .then(async data => {
-        const api = google.sheets({ version: 'v4', auth: data.client });
-        const getValues = promisify(api.spreadsheets.values.batchGet.bind(api.spreadsheets.values));
-        const values = await getValues({ spreadsheetId: process.env.GOOGLE_SHEET_ID, ranges });
-        values.data.valueRanges.forEach(range => {
-            range.values = range.values.filter(x => x.some(x => x === data.email));
-        });
-        return values.data.valueRanges;
+  getClient()
+    .then((client) => getUserEmailbyToken(client, req.body.id_token || req.query.id_token))
+    .then(async (data) => {
+      const api = google.sheets({ version: 'v4', auth: data.client });
+      const getValues = promisify(api.spreadsheets.values.batchGet.bind(api.spreadsheets.values));
+      const values = await getValues({ spreadsheetId: process.env.GOOGLE_SHEET_ID, ranges });
+      values.data.valueRanges.forEach((range) => {
+        range.values = range.values.filter((x) => x.some((x) => x === data.email));
+      });
+      return values.data.valueRanges;
     })
-    .then( values => res.status(200).send(mapValues(values)))
-    .catch(err => res.status(500).send(err.message ?? "something went wrong"));
+    .then((values) => res.status(200).send(mapValues(values)))
+    .catch((err) => res.status(500).send(err.message ?? 'something went wrong'));
 });
 
-const mapValues = (values) => values.map((tab, index) =>
-        {
-           return {
-                name: tabs[index].name,
-                values: tab.values.map(x => x.reduce((acc, value, i) => {
-                        return { ...acc, [tabs[index].fields[i]]: value };
-                    }, {})
-                )
-            }
-        });
+const mapValues = (values) =>
+  values.map((tab, index) => {
+    return {
+      name: tabs[index].name,
+      values: tab.values.map((x) =>
+        x.reduce((acc, value, i) => {
+          return { ...acc, [tabs[index].fields[i]]: value };
+        }, {}),
+      ),
+    };
+  });
 
 // Helper Functions
 const getUserEmailbyToken = async (client, token) => {
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: client.id
-    });
-    const payload = ticket.getPayload();
-    const email = payload.email;
-    return {client, email};
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: client.id,
+  });
+  const payload = ticket.getPayload();
+  const email = payload.email;
+  console.log(`${payload.name} requested vacation balance. [${email}]`);
+  return { client, email };
 };
 
 const getClient = async () => {
-    const params = {
-        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    };
+  const params = {
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  };
 
-    if (environment == 'development') {
-        const client = new google.auth.JWT(
-            process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-            null,
-            process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
-            params.scopes
-        );
-        return new Promise((resolve) => {
-            resolve(client);
-        });
-    }
-    return google.auth.getClient(params);
+  if (environment == 'development') {
+    const client = new google.auth.JWT(
+      process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      null,
+      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
+      params.scopes,
+    );
+    return new Promise((resolve) => {
+      resolve(client);
+    });
+  }
+  return google.auth.getClient(params);
 };
 
 //Spreadsheet settings
 const tabs = [
-    {
-        name: 'balance',
-        sheetName: 'BalanceConfidential',
-        rangeStart: 'A',
-        rangeEnd: 'I',
-        fields: [
-            "name",
-            "project",
-            "email",
-            "startDate",
-            "vacationBalance",
-            "vacationUsedTotal",
-            "sickDaysUsedTotal",
-            "vacationUsedLastYear",
-            "sickDaysUsedLastYear"
-        ]
-    },
-    {
-        name: 'requests',
-        sheetName: 'RequestsData',
-        rangeStart: 'A',
-        rangeEnd: 'H',
-        fields:[
-            "timestamp",
-            "email",
-            "type",
-            "project",
-            "startDate",
-            "endDate",
-            "length",
-            "comment"
-        ]
-    }
+  {
+    name: 'balance',
+    sheetName: 'BalanceConfidential',
+    rangeStart: 'A',
+    rangeEnd: 'I',
+    fields: [
+      'name',
+      'project',
+      'email',
+      'startDate',
+      'vacationBalance',
+      'vacationUsedTotal',
+      'sickDaysUsedTotal',
+      'vacationUsedLastYear',
+      'sickDaysUsedLastYear',
+    ],
+  },
+  {
+    name: 'requests',
+    sheetName: 'RequestsData',
+    rangeStart: 'A',
+    rangeEnd: 'H',
+    fields: ['timestamp', 'email', 'type', 'project', 'startDate', 'endDate', 'length', 'comment'],
+  },
 ];
 
-const ranges = tabs.map(x => `${x.sheetName}!${x.rangeStart}:${x.rangeEnd}`);
+const ranges = tabs.map((x) => `${x.sheetName}!${x.rangeStart}:${x.rangeEnd}`);
