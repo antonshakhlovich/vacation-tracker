7a8f86365a23613cd8252779a9a71da2a3e48e76
Anton Shakhlovich <anton.shakhlovich@gmail.com>
2023-01-10 12:28:35 +0400

    fix: build ci master only

---
+++ src/components/history.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
+function History() {
+  return;
+  <div></div>;
+}
+
+export default History;
---
+++ src/components/team.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
+function Team() {
+  return;
+  <div></div>;
+}
+
+export default Team;
